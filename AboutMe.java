0f5aac4cfb70350885c6a4eb55fce7b7bd994580
manishbeth18 <manish.beth@gmail.com>
2025-11-16 19:11:09 +0530

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,14 @@
+public class AboutMe {
+    
+    public static void main(String[] args) {
+        String name = "Manish";
+        int age = 19;
+        String language = "Java";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favorite Language: " + language);
+    }
+}
+
+
+
